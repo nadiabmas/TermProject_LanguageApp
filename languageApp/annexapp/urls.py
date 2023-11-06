b5f6c9c6607cfb2beb5a42bd47290206e9281a23
nadiabmas <82006155+nadiabmas@users.noreply.github.com>
2023-11-06 06:21:05 -0500

    Pick an option from dashboard working!

--- languageApp/annexapp/urls.py
+++ languageApp/annexapp/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('error_language/', views.error_language, name='error_language'),
+    path('lessons/', views.lessons, name='lessons'),
+    path('quizzes/', views.quizzes, name='quizzes'),
+    path('progress/', views.progress, name='progress'),
 ]
--- languageApp/annexapp/views.py
+++ languageApp/annexapp/views.py
@@ -13,3 +13,15 @@ def error_language(request):
     return render(request, 'error_language.html')
 
 
+def lessons(request):
+    return render(request, 'lessons.html')
+
+
+def quizzes(request):
+    return render(request, 'quizzes.html')
+
+
+def progress(request):
+    return render(request, 'progress.html')
+
+
