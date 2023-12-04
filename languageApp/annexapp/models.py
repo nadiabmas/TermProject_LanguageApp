ee67bf11948113eb299231855ae6a5d02d706b37
nadiabmas <82006155+nadiabmas@users.noreply.github.com>
2023-12-04 09:41:01 -0500

    Changes: added quiz, question and option models to models.py

--- languageApp/annexapp/models.py
+++ languageApp/annexapp/models.py
@@ -7,3 +7,21 @@ class UserProfile(models.Model):
     username = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=50)
 
+
+class Quiz(models.Model):
+    quiz_title = models.CharField(max_length=50)
+    quiz_description = models.TextField(max_length=350)
+
+
+class Question(models.Model):
+    question_text = models.TextField(max_length=150)
+
+
+class Option(models.Model):
+    option_text = models.CharField(max_length=150)
+    is_correct = models.BooleanField(True)
+
+
+# class UserAnswer(models.Model):
+
+
